3a3379a092a5bbe73b4f81b8de0c46d5997b1f56
Matěj Šimek <email@matejsimek.cz>
2008-11-09 21:01:37 +0000

    nahrani prazdneho projektu na SVN

---
+++ src/semestralniprace1_lode/Main.java
@@ -0,0 +1,26 @@
+/**
+ *
+ * @author Matej Simek, 3.11.2008
+ *         CVUT FEL, matej.simek@gmail.com
+ * 
+ *                      #######################
+ *                      #  SEMESTRALNI PRACE  #
+ *                      #######################
+ * 
+ * termin odevzdani: 31.1.2009
+ * vybrane tema: hra Lode
+ * 
+ */
+
+package semestralniprace1_lode;
+import java.util.*;
+
+public class Main {
+
+        public static void main(String[] args) {
+            
+            
+            
+    }
+
+}
