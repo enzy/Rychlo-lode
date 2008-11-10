2bc321ec393ba538a4274de8e5912fe9390f0e13
Matěj Šimek <email@matejsimek.cz>
2008-11-10 17:27:21 +0000

    myslenkovy rozbor 1

--- src/semestralniprace1_lode/Main.java
+++ src/semestralniprace1_lode/Main.java
@@ -9,6 +9,32 @@
  * 
  * termin odevzdani: 31.1.2009
  * vybrane tema: hra Lode
+ * pozadavky: prace se souborem
+ * 
+ * myslenkovy rozbor:
+ * 
+ * rozmery hraciho pole?? >> pro jednoduchost pevne, odhaduji okolo min. 20x20
+ * = 400 poli s poctem lodi volitelny v rozmezi 5-100 (zaplneni 1.25% - 25%)
+ *      >>  dvou rozmerne pole (x,y), jako dat. typ bych volil bud boolean anebo
+ *          byte, pro rozliseni vice stavu pole (voda, lod, zasah apod.)
+ *          a to cele 2x (hrac + protihrac)
+ * 
+ * druha strana / protihrac by byl pocitac = nahodne generovani cisel, pripadne
+ * kdyz zbyde cas a nebude se to komplikovat, tak clovek
+ * 
+ * typ lodi bych ponechal na jednom, nevidim duvod pro vice typu, grafiku uz
+ * hotovou mam z minula
+ * 
+ * zpusob zobrazovani, pokud to pujde rozumne, bych volil vykreslovani do okna
+ * pomoci nejakych rutin, velikost bych nechal fixni, pripadne by se hraci plocha
+ * pouze centrovala (posun pocatku)
+ * v nouzi se to muze vykreslovat do sout, ale opravdu jen v nouzi, protoze se mi
+ * to nelibi :D
+ * 
+ * 
+ * 
+ * 
+ * 
  * 
  */
 
